ec59aadbe18d0fb36950c9e940566073f57d0ec6
AngelYael962 <AngelDominguez962@gmail.com>
2023-03-16 23:51:52 -0600

    commit inicial

--- ionic-basic/src/app/inicio/inicio.page.spec.ts
+++ ionic-basic/src/app/inicio/inicio.page.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { InicioPage } from './inicio.page';
 
 describe('InicioPage', () => {
