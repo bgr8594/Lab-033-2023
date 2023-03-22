082076c9f93bf97f60d940ca6ff9ac51fd5a8c3e
David Rey <75282568+david-r2002@users.noreply.github.com>
2023-03-21 20:09:27 -0600

    practica 6

--- ionic-basic/src/environments/environment.prod.ts
+++ ionic-basic/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDIg57tezJkcjzDw88Lxgh1vVnWM4t8Qt8",
+  authDomain: "appmlaboratorio-033.firebaseapp.com",
+  projectId: "appmlaboratorio-033",
+  storageBucket: "appmlaboratorio-033.appspot.com",
+  messagingSenderId: "401755382770",
+  appId: "1:401755382770:web:380e9a6f9e19b701b3816b"
+  }
 };
