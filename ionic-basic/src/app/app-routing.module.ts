3c6dadcf12c7310c23bbc9ad3fc541f7cb6622c6
AngelYael962 <AngelDominguez962@gmail.com>
2023-04-11 00:09:41 -0600

    Practica#6

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -35,6 +35,15 @@ const routes: Routes = [
     path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+
 
 ];
 
--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebaseConfig: {
+    apiKey: "AIzaSyB9z7W343WKWnK7ka96LryTce4Cm4ew9GA",
+    authDomain: "ionic-basic-2023734.firebaseapp.com",
+    projectId: "ionic-basic-2023734",
+    storageBucket: "ionic-basic-2023734.appspot.com",
+    messagingSenderId: "224916027426",
+    appId: "1:224916027426:web:912ebdadb6b1e5f18d727b"
+  }
 };
 
 /*
