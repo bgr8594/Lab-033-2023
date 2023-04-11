8fcfd13fc2d6ef1d54cecf8cb85d945e9a7bbaee
AngelYael962 <AngelDominguez962@gmail.com>
2023-04-11 16:48:17 -0600

    Practica#7

--- ionic-basic/src/app/register/register.module.ts
+++ ionic-basic/src/app/register/register.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RegisterPageRoutingModule } from './register-routing.module';
 import { RegisterPage } from './register.page';
@@ -10,7 +10,8 @@ import { RegisterPage } from './register.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    RegisterPageRoutingModule
+    RegisterPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [RegisterPage]
 })
