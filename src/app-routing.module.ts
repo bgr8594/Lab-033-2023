ac12eda0237fb9340dfb1b5a42a137585d3d5077
danielrios.exe <danielrios.exe@hotmail.com>
2023-04-14 23:45:20 -0500

    feat: practica 5

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () =>
-      import('./home/home.module').then((m) => m.HomePageModule),
+      import('./app/home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
@@ -15,41 +15,43 @@ const routes: Routes = [
   {
     path: 'presupuesto',
     loadChildren: () =>
-      import('./presupuesto/presupuesto.module').then(
+      import('./app/presupuesto/presupuesto.module').then(
         (m) => m.PresupuestoPageModule
       ),
   },
   {
     path: 'alumnos',
     loadChildren: () =>
-      import('./alumnos/alumnos.module').then((m) => m.AlumnosPageModule),
+      import('./app/alumnos/alumnos.module').then((m) => m.AlumnosPageModule),
   },
   {
     path: 'inicio',
     loadChildren: () =>
-      import('./inicio/inicio.module').then((m) => m.InicioPageModule),
+      import('./app/inicio/inicio.module').then((m) => m.InicioPageModule),
   },
   {
     path: 'receptor',
     loadChildren: () =>
-      import('./receptor/receptor.module').then((m) => m.ReceptorPageModule),
+      import('./app/receptor/receptor.module').then(
+        (m) => m.ReceptorPageModule
+      ),
   },
   {
     path: 'receta',
     loadChildren: () =>
-      import('./receta/receta.module').then((m) => m.RecetaPageModule),
+      import('./app/receta/receta.module').then((m) => m.RecetaPageModule),
   },
   {
     path: 'detalle-receta',
     loadChildren: () =>
-      import('./detalle-receta/detalle-receta.module').then(
+      import('./app/detalle-receta/detalle-receta.module').then(
         (m) => m.DetalleRecetaPageModule
       ),
   },
   {
     path: 'tabs',
     loadChildren: () =>
-      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+      import('./app/tabs/tabs.module').then((m) => m.TabsPageModule),
   },
 ];
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB8KwWMxg7tQZzfSk3GIQGnIpmhKfHPcuU',
+    authDomain: 'login-ng-87fe0.firebaseapp.com',
+    projectId: 'login-ng-87fe0',
+    storageBucket: 'login-ng-87fe0.appspot.com',
+    messagingSenderId: '499930703859',
+    appId: '1:499930703859:web:7e75ded59294b3bf361486',
+  },
 };
 
 /*
