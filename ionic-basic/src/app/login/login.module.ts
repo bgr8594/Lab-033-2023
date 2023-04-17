6ec0dba78d5d51bb1e1103b61543ae0c9a48c880
David Rey <75282568+david-r2002@users.noreply.github.com>
2023-04-17 16:05:43 -0600

    Practica 7

--- ionic-basic/src/app/login/login.module.ts
+++ ionic-basic/src/app/login/login.module.ts
@@ -1,7 +1,6 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -15,7 +14,8 @@ import { ModalErrorComponent } from '../component/modal-error/modal-error.compon
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginPage, ModalErrorComponent]
 })
