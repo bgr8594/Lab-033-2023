4686980aef72a326208d82fb20a299189712fd5f
chris <crisurbina7@gmail.com>
2023-04-20 14:34:38 -0600

    practica_7

---
+++ practica_7/ionic-basic/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production:true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB9z7W343WKWnK7ka96LryTce4Cm4ew9GA",
+    authDomain: "ionic-basic-2023734.firebaseapp.com",
+    projectId: "ionic-basic-2023734",
+    storageBucket: "ionic-basic-2023734.appspot.com",
+    messagingSenderId: "224916027426",
+    appId: "1:224916027426:web:912ebdadb6b1e5f18d727b"
+  }
+};
