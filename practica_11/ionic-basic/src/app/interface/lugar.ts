e8084ade87b2e1754e328ad902b00debaed6b063
none2k <124222581+none2k@users.noreply.github.com>
2023-05-19 16:22:18 -0600

    practicas lab
    practicas lab

---
+++ practica_11/ionic-basic/src/app/interface/lugar.ts
@@ -0,0 +1,22 @@
+export class Lugar {
+    nombre: string;
+    id?: string;
+	latitud: number = 0;
+	longitud: number = 0;
+
+    ubicacion?: any = {latitud:'', longitud:''};
+
+    constructor(){
+
+        this.nombre='';
+
+    }
+
+    public setUbicacion(latitud: string, Longiutd: string){
+
+        this.ubicacion.latitud= latitud;
+
+        this.ubicacion.longitud = Longiutd;
+
+    }
+}
