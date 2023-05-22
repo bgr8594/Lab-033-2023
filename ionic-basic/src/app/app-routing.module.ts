2adb7a614e80477eba52ae474f0bc2d746c7b714
Emilio Madrid <84986154+EmilioMadrid@users.noreply.github.com>
2023-05-21 22:48:13 -0600

    Practica 10

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -54,6 +54,10 @@ const routes: Routes = [
         path: 'destinos',
         loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule),
         canActivate: [AutGuardGuard]
+      },
+      {
+        path: 'destinos-api',
+        loadChildren: () => import('./destinos-api/destinos-api.module').then( m => m.DestinosApiPageModule)
       }
     ],
     canActivate: [AutGuardGuard]
@@ -65,7 +69,13 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'destinos-api',
+    loadChildren: () => import('./destinos-api/destinos-api.module').then( m => m.DestinosApiPageModule)
   }
+
+  
 ];
 
 @NgModule({
