1120091bd6e34dfe5a6ac27bb90ac974244b8055
Karlo Jair <karlo-sonic1@hotmail.com>
2023-05-21 18:56:29 -0600

    Practica 12

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -18,42 +18,34 @@ const routes: Routes = [
       {
         path: 'presupuesto',
         loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule),
-        canActivate: [AutGuardGuard]
       },
       {
         path: 'alumnos',
         loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule),
-        canActivate: [AutGuardGuard]
       },
       {
         path: 'inicio',
         loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule),
-        canActivate: [AutGuardGuard]
       },
       {
         path: 'receptor',
         loadChildren: () => import('./receptor/receptor.module').then( m => m.ReceptorPageModule),
-        canActivate: [AutGuardGuard]
       },
       {
         path: 'receta',
         loadChildren: () => import('./receta/receta.module').then( m => m.RecetaPageModule),
-        canActivate: [AutGuardGuard]
       },
       {
         path: 'detalle-receta',
         loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule),
-        canActivate: [AutGuardGuard]
       },
       {
         path: 'tabs',
         loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule),
-        canActivate: [AutGuardGuard]
       },
       {
         path: 'destinos',
         loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule),
-        canActivate: [AutGuardGuard]
       },
       {
         path: 'destinos-api',
