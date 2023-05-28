904302e46d46722f6932cd880b02bb6fd976d496
Mario Alberto Ordaz Flores <85772409+MarioOrdzCr@users.noreply.github.com>
2023-05-28 13:22:48 -0600

    Practica 7

--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -1,16 +1,21 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const environment = {
-  production: false
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAR7bPv54HFST7SrqJ2rI9qrbyJUOYWXYg",
+  authDomain: "laboratorio-033-maof.firebaseapp.com",
+  projectId: "laboratorio-033-maof",
+  storageBucket: "laboratorio-033-maof.appspot.com",
+  messagingSenderId: "275190811056",
+  appId: "1:275190811056:web:443e9ffb52b7e932c5ae75",
+  measurementId: "G-90T5PV26ZS"
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
