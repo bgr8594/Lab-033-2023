523a1b2f12547c8f6e5aa559a0ec16c96b736c24
Mario Alberto Ordaz Flores <85772409+MarioOrdzCr@users.noreply.github.com>
2023-05-28 12:47:22 -0600

    Practica 6

--- ionic-basic/src/app/app.routes.ts
+++ ionic-basic/src/app/app.routes.ts
@@ -1,13 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-];
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FloatMenuComponent } from './componentes/float-menu.component';
+
+@NgModule({
+  declarations: [AppComponent, FloatMenuComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  providers: [HttpClientModule,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
